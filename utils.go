30deaf17175eae5f98e3c1889b60a7fa18132e72
abhi9560 <54478454+abhi9560@users.noreply.github.com>
2024-01-17 15:21:50 +0530

    Update utils.go
    Add proper error handling for database connections and queries to ensure any potential issues are identified.

--- utils.go
+++ utils.go
@@ -8,6 +8,14 @@ import (
 // createConnection creates a connection to mysql database
 func createConnection() *sql.DB {
 	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/test")
-	fmt.Println("sql open " + err.Error())
-	return db
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
